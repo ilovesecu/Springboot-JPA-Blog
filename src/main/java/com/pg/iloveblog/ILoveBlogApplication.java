bc52a45af829650115c2e447a4f2a3402b6d16cb
ilovesecu <ilovesecu@gmail.com>
2021-08-01 01:49:49 +0900

    환경설정 완료

---
+++ src/main/java/com/pg/iloveblog/ILoveBlogApplication.java
@@ -0,0 +1,13 @@
+package com.pg.iloveblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILoveBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILoveBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pg/iloveblog/ILoveBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg.iloveblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILoveBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
