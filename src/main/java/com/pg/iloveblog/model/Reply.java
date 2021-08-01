595ce7dad4fe104581c423eb61f7724ef3e8d91a
ilovesecu <ilovesecu@gmail.com>
2021-08-02 01:06:10 +0900

    reply모델 클래스를 통한 reply 테이블 생성

--- src/main/java/com/pg/iloveblog/model/Reply.java
+++ src/main/java/com/pg/iloveblog/model/Reply.java
@@ -1,5 +1,50 @@
 package com.pg.iloveblog.model;
 
-public class Reply {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Reply {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int no;
+	
+	@Column(nullable = false, length = 200)
+	private String content; //댓글 내용
+	
+	@ManyToOne
+	@JoinColumn(name="userId", nullable = false)
+	User user; //어떤 사용자가 작성하였는가?
+	
+	@ManyToOne
+	@JoinColumn(name="boardId")
+	Board board; //어떤 글의 댓글인가?
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+	
+	@UpdateTimestamp
+	private Timestamp updateDate;
+	
+	
 }
