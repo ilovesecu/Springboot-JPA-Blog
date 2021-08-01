e648aeb1a7cf78d8a868f5ed97ae7e17696f2bbd
ilovesecu <ilovesecu@gmail.com>
2021-08-02 01:00:50 +0900

    Board 모델 클래스를 통해 Mariadb 테이블 생성

--- src/main/java/com/pg/iloveblog/model/Board.java
+++ src/main/java/com/pg/iloveblog/model/Board.java
@@ -1,5 +1,52 @@
 package com.pg.iloveblog.model;
 
-public class Board {
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Board {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int no;
+	
+	@Column(nullable = false, length = 50)
+	private String title; //제목
+	
+	@Lob //대용량 데이터
+	private String content; //섬머노트를 사용할 예정이기에 매우 길수도있음.
+	
+	@ColumnDefault("0") //문자였으면 작은 따옴표 필요
+	private int count; //조회수
+	
+	@JoinColumn(name="userId", nullable = false) //FK로 userId를 생성해주세요
+	@ManyToOne //Many:board, One:User
+	private User user; //누가 이 글을 적었는지
+	
+	@CreationTimestamp //insert될때 현재 시간이 자동으로 들어가도록 설정
+	private Timestamp createDate;
+	
+	@LastModifiedDate
+	private LocalDateTime updateDate;
 }
